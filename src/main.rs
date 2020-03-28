65eb8f2c0cbf053ea24cd873c4b466ff0ea8cdb1
Illia Chub <dragonhaze@ilchub.net>
2020-03-28 17:56:35 +0200

    Enchanced return key handling

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,35 @@ fn main() {
                           .get_matches();
     let timevalue;
     let timeout;
+    let prereturn: u8;
+    let postreturn: u8;
+    let mut i: u8 = 0;
+    let mut j: u8 = 0;
     println!("Please define timeout before start: ");
     timevalue = read!();
     timeout = time::Duration::from_secs(timevalue);
+    println!("Please define returns before keys: ");
+    prereturn = read!();
+    println!("Please define returns after keys: ");
+    postreturn = read!();
     println!("Please define keys to simulate: ");
     let keys: String = read!("{}\n");
     thread::sleep(timeout);
     loop {
-    enigo.key_sequence(&keys);
+        if prereturn != 0 {
+             while i < prereturn {
+                enigo.key_down(Key::Return);
+                enigo.key_up(Key::Return);
+                i+=1;
+            }
+        }
+        i = 0;
+        enigo.key_sequence(&keys);
+            while j < postreturn {
+                enigo.key_down(Key::Return);
+                enigo.key_up(Key::Return);
+                j+=1;
+        }
+        j = 0;
     }
 }
